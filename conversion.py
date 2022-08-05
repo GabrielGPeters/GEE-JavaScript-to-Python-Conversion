6a54d58e28db57994482c360e75a054af627a1d4
Gabriel Peters <ggp2366@rit.edu>
2022-08-05 13:00:33 -0400

    Add files via upload

--- conversion.py
+++ conversion.py
@@ -1110,7 +1110,7 @@ if __name__ == "__main__":
     work_dir = os.path.join(os.path.expanduser("~"), "geemap")
     # Get Earth Engine JavaScript examples. There are five examples in the geemap package data folder.
     # Change js_dir to your own folder containing your Earth Engine JavaScripts, such as js_dir = '/path/to/your/js/folder'
-    js_dir = 'Javascripts'
+    js_dir = 'ConversionFolder'
 
     # Convert all Earth Engine JavaScripts in a folder recursively to Python scripts.
     js_to_python_dir(in_dir=js_dir, out_dir=js_dir, use_qgis=True)
